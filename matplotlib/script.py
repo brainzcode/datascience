3acd02ffc3d17e7c746167bd24f5d7c27ec60f24
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-04-22 16:25:36 +0100

    adding population sizes to scatter graph

--- matplotlib/script.py
+++ matplotlib/script.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from data import year, pop, life_exp, life_exp1950, gdp_cap
 
 # print(year[-1])
@@ -14,27 +15,47 @@ from data import year, pop, life_exp, life_exp1950, gdp_cap
 
 
 # Basic scatter plot, log scale
-plt.scatter(gdp_cap, life_exp)
-plt.xscale('log')
+# plt.scatter(gdp_cap, life_exp, s=np_pop)
+# plt.xscale('log')
 
 # Strings
-xlab = 'GDP per Capita [in USD]'
-ylab = 'Life Expectancy [in years]'
-title = 'World Development in 2007'
+# xlab = 'GDP per Capita [in USD]'
+# ylab = 'Life Expectancy [in years]'
+# title = 'World Development in 2007'
 
 # Add axis labels
-plt.xlabel(xlab)
-plt.ylabel(ylab)
+# plt.xlabel(xlab)
+# plt.ylabel(ylab)
 
 # Add title
-plt.title(title)
+# plt.title(title)
 
 # Definition of tick_val and tick_lab
-tick_val = [1000, 10000, 100000]
-tick_lab = ['1k', '10k', '100k']
+# tick_val = [1000, 10000, 100000]
+# tick_lab = ['1k', '10k', '100k']
 
 # Adapt the ticks on the x-axis
-plt.xticks(tick_val, tick_lab)
+# plt.xticks(tick_val, tick_lab)
+
 
 # After customizing, display the plot
+# plt.show()
+
+# Store pop as a numpy array: np_pop
+np_pop = np.array(pop)
+
+# Double np_pop
+np_pop = np_pop * 2
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s=np_pop)
+
+# Previous customizations
+plt.xscale('log')
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000], ['1k', '10k', '100k'])
+
+# Display the plot
 plt.show()
