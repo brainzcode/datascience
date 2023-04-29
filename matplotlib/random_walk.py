884b51ef42f9de74e24e7109ccb623039a706f7e
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-04-29 11:14:41 +0100

    random walk example update

--- matplotlib/random_walk.py
+++ matplotlib/random_walk.py
@@ -1,8 +1,24 @@
 import numpy as np
+
 np.random.seed(234)
 
-tails = [0]
-for num in range(10):
-    coin = np.random.randint(0, 2)
-    tails.append(tails[num] + coin)
-print(tails)
+final_tails = []
+
+for num in range(100):
+    tails = [0]
+    for num in range(10):
+        coin = np.random.randint(0, 2)
+        tails.append(tails[num] + coin)
+    final_tails.append(tails[-1])
+    print(tails)
+print(final_tails)
+
+
+# import numpy as np
+# np.random.seed(234)
+
+# tails = [0]
+# for num in range(10):
+#     coin = np.random.randint(0, 2)
+#     tails.append(tails[num] + coin)
+# print(tails)
