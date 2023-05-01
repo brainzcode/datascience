b0099d3e798de942bc00167bd6111b641c423b7a
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-01 22:40:17 +0100

    subsetting columns

---
+++ pandas/subsetting_columns.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+homelessness = pd.read_csv("datasets/homelessness.csv")
+
+# Select only the individuals and state columns, in that order
+ind_state = homelessness[['individuals', 'state']]
+
+# Print the head of the result
+print(ind_state.head())
