fac656927837e501697fc62d4868c58252d449ef
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-01 22:37:08 +0100

    inspect dataframe

---
+++ pandas/inspect_dataframe.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+homelessness = pd.read_csv("datasets/homelessness.csv")
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
