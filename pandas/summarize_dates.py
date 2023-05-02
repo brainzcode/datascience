2111d84d457870a901a8232cdc149d9da2fb1e5b
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-02 22:46:48 +0100

    summarize dates

---
+++ pandas/summarize_dates.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+sales = pd.read_csv("datasets/sales.csv")
+
+
+# Print the maximum of the date column
+print(sales['date'].max())
+
+# Print the minimum of the date column
+print(sales['date'].min())
