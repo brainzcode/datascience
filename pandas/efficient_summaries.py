ab62bf6534c9d205ca285716dc9c5b77287a5e4e
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-02 22:47:11 +0100

    efficient summaries

---
+++ pandas/efficient_summaries.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+sales = pd.read_csv("datasets/sales.csv")
+
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+
+# Print IQR of the temperature_c column
+print(sales['temperature_c'].agg(iqr))
