f93498ed3f8e176497fb3cbd772d0f9b7d636618
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-03 10:49:48 +0100

    fill empty values and sum

---
+++ pandas/fill_sum.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+sales = pd.read_csv("datasets/sales_1_1.csv")
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values='weekly_sales',
+      index='type', columns='department', fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department",
+      columns="type", fill_value=0, margins=True))
