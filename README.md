f564d3bfdb66e9732134da5362b722131d5b92b5
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-04 16:51:00 +0100

    sorting by index values

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Data Science Practice.py
+
+- This a repo created for documentation and reference purposes of my learnings on DataCamp.
+- All materials here belong to DataCamp
+- You're free to make contributions to this repo if you wish.
---
+++ pandas/slicing_indexing/sort_index_values.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+temperatures = pd.read_csv("datasets/temperatures.csv")
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(['country', 'city'])
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level='city'))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(
+    by=['country', 'city'], ascending=[False, True]))
