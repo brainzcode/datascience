11607992baac5665666cfed379992f5ba0933677
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-07 11:58:37 +0100

    Subsetting pivot tables

---
+++ pandas/slicing_indexing/subset_pivot_table.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+temperatures = pd.read_csv("datasets/temperatures.csv")
+
+# Pivot avg_temp_c by country and city vs year
+temp_by_country_city_vs_year = temperatures.pivot_table(
+    'avg_temp_c', index=['country', 'city'], columns='year')
+
+# Subset for Egypt to India
+print(temp_by_country_city_vs_year.loc['Egypt':'India'])
+
+# Subset for Egypt, Cairo to India, Delhi
+print(temp_by_country_city_vs_year.loc[('Egypt', 'Cairo'):('India', 'Delhi')])
+
+# Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+print(temp_by_country_city_vs_year.loc[(
+    'Egypt', 'Cairo'):('India', 'Delhi'), '2005':'2010'])
