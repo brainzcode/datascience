482d1594af817027110d5c87d7453e8205c9380d
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-05-07 11:53:41 +0100

    Pivot temperature by city and year

---
+++ pandas/slicing_indexing/pivot_temp.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+temperatures = pd.read_csv("datasets/temperatures.csv")
+
+# Add a year column to temperatures
+temperatures['year'] = temperatures['date'].dt.year
+
+# Pivot avg_temp_c by country and city vs year
+temp_by_country_city_vs_year = temperatures.pivot_table(
+    'avg_temp_c', index=['country', 'city'], columns='year')
+
+# See the result
+print(temp_by_country_city_vs_year)
